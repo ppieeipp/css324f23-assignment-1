6dc4e74e1d1acc7a7a7be4dea00abb56a335b88a
Luksika Chaitrakulthong <pieluksika@gmail.com>
2023-09-11 23:50:16 +0700

    change name and sid

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Natthaya Jitsopeepong", "sid": "6422782688"},
+    {"name": "Luksika Chaitrakulthong", "sid": "6422782696"},
 ]
